2a179306b9dd51bf4ccc67f7b154d2c015236f24
Yvs08 <yveslambenou@gmail.com>
2021-11-30 16:33:39 +0100

    Delete target directory

--- target/classes/public/script/js/app/app.js
+++
@@ -1,9 +0,0 @@
-
-(function () {
-    
-    loadScript('/script/js/angular-cookies.js');
-    loadScript('/script/js/angular-resource.js');
-    loadScript('/script/js/angular-route.js');
-    loadScript('/script/js/angular.js');
-     loadScript('http://ajax.googleapis.com/ajax/libs/angularjs/1.3.14/angular.min.js');
-})();
